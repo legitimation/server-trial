ad121562cc8b427a9d066d06bb6b942efe688ccc
Zieho Choi <jeff426@naver.com>
2021-07-09 17:01:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/server_trial/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.server_trial;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1;
+    TextView textView1;
+    EditText edtext_id;
+    static RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView1 = findViewById(R.id.textView1);
+        button1 = findViewById(R.id.button1);
+        edtext_id=findViewById(R.id.editTextID);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strId = edtext_id.getText().toString();
+                try {
+                    requestProcess();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        // RequestQueue 객체생성
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+    }
+
+    public void requestProcess() throws JSONException {
+//        String url = "http://172.10.18.185/users";
+        String url = "http://192.249.18.185/users";
+        JSONObject testjson = new JSONObject();
+        try {
+            String strId = edtext_id.getText().toString();//ALLEN
+
+            testjson.put("id", strId);
+            String jsonString = testjson.toString(); //완성된 json 포맷
+
+            //이제 전송해볼까요?
+            final RequestQueue requestQueue = Volley.newRequestQueue(this);
+            final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, testjson, new Response.Listener<JSONObject>() {
+
+                //데이터 전달을 끝내고 이제 그 응답을 받을 차례입니다.
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        Log.i("response", "데이터 전송 성공");
+
+                        //받은 json형식의 응답을 받아
+                        JSONObject jsonObject = new JSONObject(response.toString());
+
+                        //key값에 따라 value값을 쪼개 받아옵니다.
+                        String resultId = jsonObject.getString("approve_id");
+
+
+                        Log.i("approve_id", resultId);
+                        textView1.setText(resultId);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                //서버로 데이터 전달 및 응답 받기에 실패한 경우 아래 코드가 실행됩니다.
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                    Log.i("failed", "failed connecting server");
+                    //Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+            requestQueue.add(jsonObjectRequest);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
